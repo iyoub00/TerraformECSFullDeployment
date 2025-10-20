3e50351fc4bd70a4c96bb48883fdda98c382df37
Ayoub Nassiri <121341933+iyoub00@users.noreply.github.com>
2025-10-20 10:27:13 +0100

    fix: cf deploy 1

--- hello-ecr/scripts/run_cfn_min.sh
+++ hello-ecr/scripts/run_cfn_min.sh
@@ -2,11 +2,21 @@
 set -euo pipefail
 
 : "${STACK_NAME:=gha-ec2}"
-: "${KEY_NAME:?missing KEY_NAME}"          # existing EC2 key pair (keep name in a secret)
+: "${KEY_NAME:?missing KEY_NAME}"
 : "${INSTANCE_TYPE:=t3.micro}"
 : "${APP_PORT:=5000}"
 : "${YOUR_IP_CIDR:=0.0.0.0/0}"
 : "${AWS_REGION:=eu-west-1}"
+
+# Resolve latest Amazon Linux 2023 x86_64 AMI (official owner 137112412989)
+AMI_ID=$(aws ec2 describe-images \
+  --region "$AWS_REGION" \
+  --owners 137112412989 \
+  --filters "Name=name,Values=al2023-ami-*-x86_64" "Name=state,Values=available" \
+  --query "Images | sort_by(@, &CreationDate)[-1].ImageId" \
+  --output text)
+
+echo "[cfn] Using AMI $AMI_ID"
 
 aws cloudformation deploy \
   --region "$AWS_REGION" \
@@ -18,11 +28,11 @@ aws cloudformation deploy \
     InstanceType="$INSTANCE_TYPE" \
     AppPort="$APP_PORT" \
     YourIpCidr="$YOUR_IP_CIDR" \
+    AmiId="$AMI_ID" \
   --capabilities CAPABILITY_IAM
 
 PUBLIC_IP=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --region "$AWS_REGION" \
   --query "Stacks[0].Outputs[?OutputKey=='PublicIp'].OutputValue" --output text)
 
 echo "PUBLIC_IP=${PUBLIC_IP}"
-# If used in GitHub Actions:
 echo "PUBLIC_IP=${PUBLIC_IP}" >> "$GITHUB_OUTPUT"
